2923611388fbe11e6559696cf6ee5405c82d25f2
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2014-09-08 17:27:03 +0200

    Initial Commit

---
+++ GNCollections/GNCollections.h
@@ -0,0 +1,13 @@
+//
+//  GNCollections.h
+//  GNCollections
+//
+//  Created by Jakub Knejzlik on 08/09/14.
+//  Copyright (c) 2014 Jakub Knejzlik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GNCollections : NSObject
+
+@end
---
+++ GNCollections/GNCollections.m
@@ -0,0 +1,13 @@
+//
+//  GNCollections.m
+//  GNCollections
+//
+//  Created by Jakub Knejzlik on 08/09/14.
+//  Copyright (c) 2014 Jakub Knejzlik. All rights reserved.
+//
+
+#import "GNCollections.h"
+
+@implementation GNCollections
+
+@end
